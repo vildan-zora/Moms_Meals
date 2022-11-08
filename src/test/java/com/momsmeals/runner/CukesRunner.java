5494a97722bc63156c1a0c800bd79864baed8cad
vildanzora <orgunvildan@gmail.com>
2022-11-08 09:02:37 -0600

    commit

--- src/test/java/com/momsmeals/runner/CukesRunner.java
+++ src/test/java/com/momsmeals/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/momsmeals/step_definitions",
         dryRun = false,
-        tags =  "@done"
+        tags =  ""
 )
 public class CukesRunner {
 }
